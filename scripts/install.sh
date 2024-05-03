174d917babc0fea6fb07e261e248f475e84ef3f7
John Whitson <john.whitson@gmail.com>
2024-05-03 14:37:50 -0600

    Added installation script

---
+++ scripts/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+TARGET_BASE_DIR=/opt/envoi
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y ruby-dev
+sudo apt-get install -y build-essential
+sudo gem install bundler
+sudo gem install aspera-cli
+sudo mkdir -p "${TARGET_BASE_DIR}"
+sudo chown -R ubuntu:ubuntu "${TARGET_BASE_DIR}"
+cd "${TARGET_BASE_DIR}" || exit 1
+git clone https://github.com/envoi-io/envoi-transfer-service.git
+cd "${TARGET_BASE_DIR}/envoi-transfer-service/src/envoi-transfer-worker" || exit 1
+sudo bundle install
+ruby install-worker-service.rb
